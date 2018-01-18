8d13b33aab6c529ce733a9e010f241077ad5ecba
xuesongcui <m17301071346@163.com>
2018-01-18 18:56:04 +0800

    socket连接修改

--- src/main/java/com/reptile/contorller/TalkframeController.java
+++ src/main/java/com/reptile/contorller/TalkframeController.java
@@ -1,71 +1,71 @@
-package com.reptile.contorller;
-
-
-
-import net.sf.json.JSONObject;
-import org.springframework.stereotype.Component;
-
-import javax.websocket.OnClose;
-import javax.websocket.OnMessage;
-import javax.websocket.OnOpen;
-import javax.websocket.Session;
-import javax.websocket.server.ServerEndpoint;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
-/**
- * Created by Administrator on 2017/5/19.
- */
-@Component
-@ServerEndpoint("/hello")
-public class TalkframeController {
-	private static  Map<String,Session> wsUserMap = new HashMap<String,Session>();
-	private static  Map<String,String> wsInfoMap = new HashMap<String,String>();
-	
-    public static Map<String, String> getWsInfoMap() {
-		return wsInfoMap;
-	}
-	public static void setWsInfoMap(Map<String, String> wsInfoMap) {
-		TalkframeController.wsInfoMap = wsInfoMap;
-	}
-	public static Map<String, Session> getWsUserMap() {
-		return wsUserMap;
-	}
-	public static void setWsUserMap(Map<String, Session> wsUserMap) {
-		TalkframeController.wsUserMap = wsUserMap;
-	}
-	@OnOpen
-    public void onopen(Session session){
-        System.out.println("连接成功");
-    }
-    @OnClose
-    public void onclose(Session session){
-		try {
-			session.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		System.out.println("close....");
-    }
-    @OnMessage
-    public void onsend(Session session,String msg){
-    	System.out.println("msg===="+msg);
-    	if(msg!=null&&!msg.equals("")){
-    		try {
-    			System.out.println("msg===="+msg);
-				session.getBasicRemote().sendText(msg.substring(0, msg.length()-1).toString()+",\"resultCode\":\"1111\"}");//链接成功
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-    		JSONObject json=JSONObject.fromObject(msg);
-    		msg=json.get("req").toString();
-    		wsUserMap.put(msg, session);
-    		wsInfoMap.put(msg, json.get("seq_id").toString());
-    	}else{
-    		onclose(session);
-    	}
-    }
-    
-    
-}
+//package com.reptile.contorller;
+//
+//
+//
+//import net.sf.json.JSONObject;
+//import org.springframework.stereotype.Component;
+//
+//import javax.websocket.OnClose;
+//import javax.websocket.OnMessage;
+//import javax.websocket.OnOpen;
+//import javax.websocket.Session;
+//import javax.websocket.server.ServerEndpoint;
+//import java.io.IOException;
+//import java.util.HashMap;
+//import java.util.Map;
+//
+///**
+// * Created by Administrator on 2017/5/19.
+// */
+//@Component
+//@ServerEndpoint("/hello")
+//public class TalkframeController {
+//	private static  Map<String,Session> wsUserMap = new HashMap<String,Session>();
+//	private static  Map<String,String> wsInfoMap = new HashMap<String,String>();
+//	
+//    public static Map<String, String> getWsInfoMap() {
+//		return wsInfoMap;
+//	}
+//	public static void setWsInfoMap(Map<String, String> wsInfoMap) {
+//		TalkframeController.wsInfoMap = wsInfoMap;
+//	}
+//	public static Map<String, Session> getWsUserMap() {
+//		return wsUserMap;
+//	}
+//	public static void setWsUserMap(Map<String, Session> wsUserMap) {
+//		TalkframeController.wsUserMap = wsUserMap;
+//	}
+//	@OnOpen
+//    public void onopen(Session session){
+//        System.out.println("连接成功");
+//    }
+//    @OnClose
+//    public void onclose(Session session){
+//		try {
+//			session.close();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//		System.out.println("close....");
+//    }
+//    @OnMessage
+//    public void onsend(Session session,String msg){
+//    	System.out.println("msg===="+msg);
+//    	if(msg!=null&&!msg.equals("")){
+//    		try {
+//    			System.out.println("msg===="+msg);
+//				session.getBasicRemote().sendText(msg.substring(0, msg.length()-1).toString()+",\"resultCode\":\"1111\"}");//链接成功
+//			} catch (Exception e) {
+//				e.printStackTrace();
+//			}
+//    		JSONObject json=JSONObject.fromObject(msg);
+//    		msg=json.get("req").toString();
+//    		wsUserMap.put(msg, session);
+//    		wsInfoMap.put(msg, json.get("seq_id").toString());
+//    	}else{
+//    		onclose(session);
+//    	}
+//    }
+//    
+//    
+//}
--- src/main/java/com/reptile/util/PushSocket.java
+++ src/main/java/com/reptile/util/PushSocket.java
@@ -1,5 +1,7 @@
 package com.reptile.util;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,52 +10,57 @@ import javax.websocket.Session;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.reptile.contorller.TalkframeController;
-
 public class PushSocket {
 
 	private static Logger logger = LoggerFactory.getLogger(PushSocket.class);
 
+	
 	/**
-	 * 0001 失败 0000 成功
-	 * 
+	 * 0001 失败 0000 成功 1000登陆中
+	 *
 	 * @param map
 	 * @param UUID
-	 * @param errorInfo
+	 * @param resultCode
+	 * @param errorInfor 失败原因
 	 */
-	public static Map<String, Object> push(Map<String, Object> map,
-			String UUID,String resultCode,String errorInfor) {
+	public static Map<String, Object> push(Map<String, Object> map, String UUID, String resultCode, String errorInfor) {
+
+		logger.warn(UUID + "：本次认证流程，现处于阶段为：" + errorInfor);
 		Map<String, Object> mapData = new HashMap<String, Object>();
-		Session se = TalkframeController.getWsUserMap().get(UUID);
-		String seq_id = TalkframeController.getWsInfoMap().get(UUID);
-		logger.warn("----------------长连接推送开始------se：" + se + "-----seq_id:"+ seq_id);
-		logger.warn("----------------长连接推送开始------errorInfor：" + errorInfor);
-		logger.warn("----------------*****************------UUID：" + UUID);
-		System.out.println("errorInfor----"+errorInfor);
-		String date=CountTime.currentTime();
+		Session se = LongLink.getWsUserMap().get(UUID);
+		String seq_id = LongLink.getWsInfoMap().get(UUID);
+		System.out.println("se===" + se);
+		System.out.println("seq===" + seq_id);
+		System.out.println(errorInfor + resultCode);
+		String date = currentTime();
+
+		String actionType = "oldLink";
+		if (UUID.contains("+")) {
+			logger.warn("--------------------新版本长连接。本次连接目标为："+UUID);
+			actionType = (UUID.split("\\+"))[1];
+		}
+
 		try {
 			if (se != null && seq_id != null) {
-				if (seq_id.equals("hello")) {
-					se.getBasicRemote().sendText(
-							"{\"resultCode\":\"" + resultCode
-									+ "\",\"seq_id\":\"" + seq_id + "\",\"errorInfor\":\""+errorInfor+"\",\"date\":\""+date+"\"}");
-				} else {
-					se.getBasicRemote().sendText(
-							"{\"resultCode\":" + resultCode + ",\"seq_id\":"
-									+ seq_id +",\"errorInfor\":\""+errorInfor+"\",\"date\":\""+date+ "\"}");
-				}
-
+				se.getBasicRemote().sendText("{\"resultCode\":\"" + resultCode + "\",\"seq_id\":\"" + seq_id + "\",\"errorInfor\":\"" + errorInfor + "\",\"date\":\"" + date + "\",\"actionType\":\"" + actionType + "\"}");
 			}
-			System.out.println("长连接推送成功----"+errorInfor);
-
 		} catch (Exception e) {
-			logger.warn("----------------长连接推送失败------",e);
-			map.put("errorCode", "0001");
-			map.put("errorInfo", "网络异常");
+			logger.warn("------------------------推送状态时，长链接出现问题----------------------", e);
+			map.put("errorCode", "1100");
+			map.put("errorInfo", "推送状态时，连接已关闭");
 		}
-		
 		return map;
+	}
 
+	/**
+	 * 获取当前时间（年月日时分秒）
+	 *
+	 * @return
+	 */
+	public static String currentTime() {
+		SimpleDateFormat format = new SimpleDateFormat("yyyy年MM月dd日  HH:mm:ss");
+		String mon = format.format(new Date());
+		return mon;
 	}
 
 }
